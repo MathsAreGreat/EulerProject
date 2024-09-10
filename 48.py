3c534f8948645a0d0ea685e2448ce2a1f5f3e172
MathsAreGreat <mathsphile@gmail.com>
2024-09-10 23:17:55 +0100

    first commit

---
+++ 48.py
@@ -0,0 +1,12 @@
+from sympy.ntheory import factorint
+import numpy as np
+import time
+
+startTime = time.time()
+somme = 0
+for i in range(1, 1000):
+    print(i, end="\r")
+    somme += pow(i, i, 10**10)
+
+print(somme % 10**10)
+print(time.time()-startTime)
